0174236da276c99fa0d87294dbf32d2c27931f3c
Bowen Sun <bowensun@gmail.com>
2016-07-20 21:42:11 +0100

    # This is a combination of 6 commits. # The first commit's message is: fix entrypoint not found
    # This is the 2nd commit message:
    
    up nginx version
    
    # This is the 3rd commit message:
    
    rollback nginx version causing issues.
    
    # This is the 4th commit message:
    
    fix syntax
    
    # This is the 5th commit message:
    
    bake in error page in nginx
    
    # This is the 6th commit message:
    
    use variable to configure error_page path (+2 squashed commits)
    Squashed commits:
    [5801050] fix
    [e5be026] external error page on S3

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 
 # Validate environment variables
 : "${UPSTREAM:?Set UPSTREAM using --env}"
-: "${UPSTREAM_PORT?Set UPSTREAM_PORT using --env}"
+: "${UPSTREAM_PORT:?Set UPSTREAM_PORT using --env}"
+: "${ERROR_PAGE:?Set ERROR_PAGE using --env}"
 PROTOCOL=${PROTOCOL:=HTTP}
 
 # Template an nginx.conf
 cat <<EOF >/etc/nginx/nginx.conf
 user nginx;
 worker_processes 2;
-server_tokens off;
 
 events {
   worker_connections 1024;
@@ -22,6 +22,7 @@ if [ "$PROTOCOL" = "HTTP" ]; then
 cat <<EOF >>/etc/nginx/nginx.conf
 
 http {
+  server_tokens off;
   access_log /var/log/nginx/access.log;
   error_log /var/log/nginx/error.log;
 
@@ -30,6 +31,8 @@ http {
       proxy_pass http://${UPSTREAM}:${UPSTREAM_PORT};
       proxy_set_header Host \$host;
       proxy_set_header X-Forwarded-For \$remote_addr;
+      proxy_intercept_errors on;
+      error_page 400 403 404 405 414 416 500 501 502 503 504 ${ERROR_PAGE};
     }
   }
 }
