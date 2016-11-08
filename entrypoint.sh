a5d0ba7635560346a6568c630b9845f1f3fae363
konradko <konrad.korzel@gmail.com>
2016-11-08 17:44:06 +0000

    Add HTTPS redirect, security headers and admin IP whitelisting

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ set -euo pipefail
 : "${UPSTREAM_PORT:?Set UPSTREAM_PORT using --env}"
 : "${ERROR_PAGE:?Set ERROR_PAGE using --env}"
 : "${CLIENT_MAX_BODY_SIZE:?Set CLIENT_MAX_BODY_SIZE using --env}"
+: "${ADMIN_ALLOW:?Set ADMIN_ALLOW using --env}"
+: "${ADMIN_DENY:?Set ADMIN_DENY using --env}"
 PROTOCOL=${PROTOCOL:=HTTP}
 
 # Template an nginx.conf
@@ -20,6 +22,18 @@ events {
 EOF
 
 if [ "$PROTOCOL" = "HTTP" ]; then
+
+cat <<EOF >/etc/nginx/directory_proxy.conf
+proxy_pass http://${UPSTREAM}:${UPSTREAM_PORT};
+proxy_set_header Host \$host;
+proxy_set_header X-Forwarded-For \$remote_addr;
+error_page 403 405 414 416 500 501 502 503 504 ${ERROR_PAGE};
+client_max_body_size ${CLIENT_MAX_BODY_SIZE};
+add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload";
+add_header X-Frame-Options DENY;
+add_header X-Content-Type-Options nosniff;
+EOF
+
 cat <<EOF >>/etc/nginx/nginx.conf
 
 http {
@@ -29,12 +43,19 @@ http {
 
   server {
     location / {
-      proxy_pass http://${UPSTREAM}:${UPSTREAM_PORT};
-      proxy_set_header Host \$host;
-      proxy_set_header X-Forwarded-For \$remote_addr;
-      error_page 403 405 414 416 500 501 502 503 504 ${ERROR_PAGE};
-      client_max_body_size ${CLIENT_MAX_BODY_SIZE};
+      include /etc/nginx/directory_proxy.conf;
     }
+
+    location ^~ /admin/ {
+      include /etc/nginx/directory_proxy.conf;
+      allow ${ADMIN_ALLOW};
+      deny ${ADMIN_DENY};
+    }
+
+    if ($http_x_forwarded_proto != 'https') {
+      return 301 https://\$host\$request_uri;
+    }
+
   }
 }
 EOF
